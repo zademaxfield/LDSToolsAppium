4be857eb405dd45aabd8d446c57a98f4d9ea7c52
Zade Maxfield <zade.maxfield@ldschurch.org>
2020-02-18 10:34:43 -0700

    More API files

---
+++ src/test/java/LDSToolsAppium/MemberToolsAPITest.java
@@ -0,0 +1,17 @@
+package LDSToolsAppium;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MemberToolsAPITest {
+
+    public MemberToolsAPITest() {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL %4$-7s [%3$s] %5$s %6$s%n");
+    }
+
+    @Test
+    public void testToolsService() throws Exception {
+        new MemberToolsAPI().toolsService();
+    }
+}

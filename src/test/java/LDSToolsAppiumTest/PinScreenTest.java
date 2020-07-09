bdc3068978109274b92baabdc6325b7be77e5c14
Zade Maxfield <zade.maxfield@ldschurch.org>
2020-07-09 10:48:38 -0600

    Fixed PIN tests. Still bugs in iOS and Android.

--- src/test/java/LDSToolsAppiumTest/PinScreenTest.java
+++ src/test/java/LDSToolsAppiumTest/PinScreenTest.java
@@ -13,6 +13,7 @@ import org.testng.annotations.Test;
 public class PinScreenTest extends BaseDriver {
 
     //TODO: Bug iOS is allowing repeating numbers - MMIP-5824
+    //TODO: Android bug MMA-3502
     @Test (groups = {"all1", "all"})
     public void pinRepeatTest() throws Exception {
         HelperMethods myHelper = new HelperMethods(driver);
@@ -35,6 +36,7 @@ public class PinScreenTest extends BaseDriver {
     }
 
     //TODO: Bug iOS is allowing repeating numbers - MMIP-5824
+    //TODO: Android bug MMA-3502
     @Test (groups = {"all1", "all"})
     public void pinRepeatTestNonLeader() throws Exception {
         HelperMethods myHelper = new HelperMethods(driver);
@@ -48,19 +50,18 @@ public class PinScreenTest extends BaseDriver {
         myHelper.checkForAlertsBeforePin();
         myHelper.dismissWhatsNewPage();
 
-        if (!getRunningOS().equals("ios")) {
-            if (myPinScreen.pinAlertDialogOK.isDisplayed()) {
-                myPinScreen.pinAlertDialogOK.click();
-            }
-        }
+//        if (!getRunningOS().equals("ios")) {
+//            if (myPinScreen.pinAlertDialogOK.isDisplayed()) {
+//                myPinScreen.pinAlertDialogOK.click();
+//            }
+//        }
 
         pinRepeatTestData();
     }
 
-    //TODO: Android bug MMA-3502
-    //TODO: Need to update user
-    @Test(groups = {"needUpdate", "jft"})
-//    @Test (groups = {"all2", "all"})
+
+
+    @Test (groups = {"all2", "all"})
     public void pinNoMatchTest() throws Exception {
         HelperMethods myHelper = new HelperMethods(driver);
         PinScreen myPinScreen = new PinScreen(driver);
@@ -77,35 +78,35 @@ public class PinScreenTest extends BaseDriver {
 
     }
 
-    //TODO: Need to update user
-    @Test(groups = {"needUpdate"})
-//    @Test (groups = {"all2", "all"})
+
+    @Test (groups = {"all2", "all"})
     public void pinNoMatchTestNonLeader() throws Exception {
         HelperMethods myHelper = new HelperMethods(driver);
         PinScreen myPinScreen = new PinScreen(driver);
-        myHelper.loginUAT("LDSTools5", "toolstester");
+        myHelper.proxyLogin("dcbryson");
+//        myHelper.loginUAT("LDSTools5", "toolstester");
 
         myHelper.checkForAlertsBeforePin();
         myHelper.dismissWhatsNewPage();
 
-        if (!getRunningOS().equals("ios")) {
-            if (myPinScreen.pinAlertDialogOK.isDisplayed()) {
-                myPinScreen.pinAlertDialogOK.click();
-            }
-        }
+//        if (!getRunningOS().equals("ios")) {
+//            if (myPinScreen.pinAlertDialogOK.isDisplayed()) {
+//                myPinScreen.pinAlertDialogOK.click();
+//            }
+//        }
 
         pinNoMatchTestData();
 
     }
 
 
-    //TODO: Need to update user
-    @Test(groups = {"needUpdate"})
-//    @Test (groups = {"all3", "all"})
+    //TODO: Android bug MMA-3502
+    @Test (groups = {"all3", "all"})
     public void pinSequentialTest() throws Exception {
         HelperMethods myHelper = new HelperMethods(driver);
         PinScreen myPinScreen = new PinScreen(driver);
-        myHelper.loginUAT("LDSTools3", "toolstester");
+        myHelper.proxyLogin("kroqbandit");
+//        myHelper.loginUAT("LDSTools3", "toolstester");
 //        myHelper.loginUAT("LDSTools5", "toolstester");
 
 //        if (!getRunningOS().equals("ios")) {
@@ -113,34 +114,34 @@ public class PinScreenTest extends BaseDriver {
 //        }
 
 
-        if (myPinScreen.pinAlertDialogOK.isDisplayed()) {
-            myPinScreen.pinAlertDialogOK.click();
-        }
+//        if (myPinScreen.pinAlertDialogOK.isDisplayed()) {
+//            myPinScreen.pinAlertDialogOK.click();
+//        }
 
         sequentialTestData();
 
 
     }
 
-    //TODO: Need to update user
-    @Test(groups = {"needUpdate"})
-//    @Test (groups = {"all3", "all"})
+    //TODO: Android bug MMA-3502
+    @Test (groups = {"all3", "all", "jft"})
     public void pinSequentialTestNonLeader() throws Exception {
         HelperMethods myHelper = new HelperMethods(driver);
         PinScreen myPinScreen = new PinScreen(driver);
-        myHelper.loginUAT("LDSTools5", "toolstester");
+        myHelper.proxyLogin("dcbryson");
+//        myHelper.loginUAT("LDSTools5", "toolstester");
 
-        myHelper.checkForAlertsBeforePin();
-        myHelper.dismissWhatsNewPage();
+//        myHelper.checkForAlertsBeforePin();
+//        myHelper.dismissWhatsNewPage();
 
-        if (!getRunningOS().equals("ios")) {
-
-
-            if (myPinScreen.pinAlertDialogOK.isDisplayed()) {
-                myPinScreen.pinAlertDialogOK.click();
-            }
-
-        }
+//        if (!getRunningOS().equals("ios")) {
+//
+//
+//            if (myPinScreen.pinAlertDialogOK.isDisplayed()) {
+//                myPinScreen.pinAlertDialogOK.click();
+//            }
+//
+//        }
 
 
         sequentialTestData();
@@ -151,11 +152,26 @@ public class PinScreenTest extends BaseDriver {
     private void sequentialTestData() throws Exception {
         HelperMethods myHelper = new HelperMethods(driver);
         PinScreen myPinScreen = new PinScreen(driver);
+        MenuScreen myMenu = new MenuScreen(driver);
+        SettingsScreen mySettings = new SettingsScreen(driver);
+
+        if (getRunningOS().equalsIgnoreCase("android")) {
+            myHelper.enterPin("1", "1", "3", "3");
+            myMenu.selectMenu(myMenu.settings);
+            mySettings.createAPIN.click();
+        }
+
+
+
         //Sequential Numbers
         myHelper.pressPinKeys("1");
         myHelper.pressPinKeys("2");
         myHelper.pressPinKeys("3");
         myHelper.pressPinKeys("4");
+
+        if (getRunningOS().equalsIgnoreCase("android")) {
+            myPinScreen.pinKeyEnter.click();
+        }
 
 
         //Should get an error on iOS .... Android needs to enter in the digits again to get an error
@@ -164,6 +180,10 @@ public class PinScreenTest extends BaseDriver {
             myHelper.pressPinKeys("2");
             myHelper.pressPinKeys("3");
             myHelper.pressPinKeys("4");
+
+
+            myPinScreen.pinKeyEnter.click();
+
 
             Assert.assertEquals("PIN cannot have sequential or repeating numbers.", myPinScreen.pinKeyErrorMessage.getText());
 
@@ -178,6 +198,10 @@ public class PinScreenTest extends BaseDriver {
         myHelper.pressPinKeys("7");
         myHelper.pressPinKeys("6");
 
+        if (getRunningOS().equalsIgnoreCase("android")) {
+            myPinScreen.pinKeyEnter.click();
+        }
+
 
         //Should get an error on iOS .... Android needs to enter in the digits again to get an error
         if (getRunningOS().equals("android")) {
@@ -185,6 +209,10 @@ public class PinScreenTest extends BaseDriver {
             myHelper.pressPinKeys("8");
             myHelper.pressPinKeys("7");
             myHelper.pressPinKeys("6");
+
+
+            myPinScreen.pinKeyEnter.click();
+
 
             Assert.assertEquals("PIN cannot have sequential or repeating numbers.", myPinScreen.pinKeyErrorMessage.getText());
 
@@ -238,7 +266,14 @@ public class PinScreenTest extends BaseDriver {
     private void pinRepeatTestData() throws Exception {
         HelperMethods myHelper = new HelperMethods(driver);
         PinScreen myPinScreen = new PinScreen(driver);
+        MenuScreen myMenu = new MenuScreen(driver);
+        SettingsScreen mySettings = new SettingsScreen(driver);
 
+        if (getRunningOS().equalsIgnoreCase("android")) {
+            myHelper.enterPin("1", "1", "3", "3");
+            myMenu.selectMenu(myMenu.settings);
+            mySettings.createAPIN.click();
+        }
 
         //All four of the same number
         myHelper.pressPinKeys("1");
@@ -246,12 +281,20 @@ public class PinScreenTest extends BaseDriver {
         myHelper.pressPinKeys("1");
         myHelper.pressPinKeys("1");
 
+        if (getRunningOS().equalsIgnoreCase("android")) {
+            myPinScreen.pinKeyEnter.click();
+        }
+
         //Should get an error on iOS .... Android needs to enter in the digits again to get an error
         if (getRunningOS().equals("android")) {
             myHelper.pressPinKeys("1");
             myHelper.pressPinKeys("1");
             myHelper.pressPinKeys("1");
             myHelper.pressPinKeys("1");
+
+
+            myPinScreen.pinKeyEnter.click();
+
 
             Assert.assertEquals("PIN cannot have sequential or repeating numbers.", myPinScreen.pinKeyErrorMessage.getText());
 
@@ -267,12 +310,20 @@ public class PinScreenTest extends BaseDriver {
         myHelper.pressPinKeys("6");
         myHelper.pressPinKeys("6");
 
+        if (getRunningOS().equalsIgnoreCase("android")) {
+            myPinScreen.pinKeyEnter.click();
+        }
+
         //Should get an error on iOS .... Android needs to enter in the digits again to get an error
         if (getRunningOS().equals("android")) {
             myHelper.pressPinKeys("5");
             myHelper.pressPinKeys("6");
             myHelper.pressPinKeys("6");
             myHelper.pressPinKeys("6");
+
+
+            myPinScreen.pinKeyEnter.click();
+
 
             Assert.assertEquals("PIN cannot have sequential or repeating numbers.", myPinScreen.pinKeyErrorMessage.getText());
 

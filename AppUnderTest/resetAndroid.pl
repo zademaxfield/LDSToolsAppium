b9f618d0971c2e3f8a81f2250f792ea4e4b76339
Zade Maxfield <zade.maxfield@ldschurch.org>
2020-08-19 10:40:54 -0600

    Changes to android reset

--- AppUnderTest/resetAndroid.pl
+++ AppUnderTest/resetAndroid.pl
@@ -5,6 +5,7 @@ foreach $i (@a) {
     system("adb -s " . $i . " uninstall org.lds.ldstools.dev");
     system("adb -s " . $i . " uninstall org.lds.ldstools");
     system("adb -s " . $i . " uninstall org.lds.ldstools.alpha");
-
+    system("adb -s " . $i . " uninstall io.appium.uiautomator2.server");
+    system("adb -s " . $i . " uninstall io.appium.uiautomator2.server.test");
 }
 exit(0);

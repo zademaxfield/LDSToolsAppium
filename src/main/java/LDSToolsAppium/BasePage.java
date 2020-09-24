ea940999020a02e75b77c72689aac9629c57ea49
Zade Maxfield <zade.maxfield@ldschurch.org>
2020-09-24 16:32:38 -0600

    Changed timeouts

--- src/main/java/LDSToolsAppium/BasePage.java
+++ src/main/java/LDSToolsAppium/BasePage.java
@@ -655,7 +655,7 @@ public class BasePage extends BaseDriver {
 
     public void waitUnitlTextIsGone(String myText) {
         //System.out.println("Start Checking for Element");
-        WebDriverWait wait = new WebDriverWait(driver.get(), 180);
+        WebDriverWait wait = new WebDriverWait(driver.get(), 60);
 
         if(getOS().equals("ios")) {
             wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//*[contains(@value, '" + myText + "')]")));
@@ -674,7 +674,7 @@ public class BasePage extends BaseDriver {
 
     public void waitForText(String myText) {
         //System.out.println("Start Checking for Element");
-        WebDriverWait wait = new WebDriverWait(driver.get(), 180);
+        WebDriverWait wait = new WebDriverWait(driver.get(), 60);
         if(getOS().equals("ios")) {
 //            WebElement iosElement = driver.get().findElement(By.xpath("//*[contains(@value, '" + myText + "')]"));
 //            wait.until(ExpectedConditions.textToBePresentInElement(iosElement, myText));

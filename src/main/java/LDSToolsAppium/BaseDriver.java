f30227370e4f893b84a0f1380ed7303fe66bde96
Zade Maxfield <zade.maxfield@ldschurch.org>
2020-12-16 09:09:35 -0700

    Update version

--- src/main/java/LDSToolsAppium/BaseDriver.java
+++ src/main/java/LDSToolsAppium/BaseDriver.java
@@ -549,7 +549,7 @@ public class BaseDriver extends AbstractTestNGCucumberTests {
             capabilities.setCapability("appium:newCommandTimeout", 600);
             capabilities.setCapability("appium:app", app.getAbsolutePath());
             capabilities.setCapability("appium:launchTimeout", 900000);
-            capabilities.setCapability("appium:platformVersion", "14.2");
+            capabilities.setCapability("appium:platformVersion", "14.3");
             capabilities.setCapability("appium:nativeInstrumentsLib", false);
             capabilities.setCapability("appium:clearSystemFiles", true);
             //capabilities.setCapability("allowTouchIdEnroll", true);

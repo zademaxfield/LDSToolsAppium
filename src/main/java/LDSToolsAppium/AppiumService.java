0913d7f21d9549c2469b54089b46d7ef7ad2adf5
Zade Maxfield <zmaxfield@Zades-MacBook-Pro-2.local>
2021-11-08 14:34:27 -0700

    User APPIUM_HOME envrioment var

--- src/main/java/LDSToolsAppium/AppiumService.java
+++ src/main/java/LDSToolsAppium/AppiumService.java
@@ -2,6 +2,7 @@ package LDSToolsAppium;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.Map;
 
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
@@ -15,13 +16,25 @@ public class AppiumService {
 	public static void startAppiumService(String os, int myPort) throws Exception {
 		System.out.println("OS: " + os);
 		System.out.println("PORT: " + myPort);
+		String appiumPath = null;
 
-		//Setup the service
+		Map<String, String> map = System.getenv();
+		for (Map.Entry <String, String> entry: map.entrySet()) {
+//			System.out.println("Variable Name:- " + entry.getKey() + " Value:- " + entry.getValue());
+			if (entry.getKey().equals("APPIUM_HOME")) {
+				appiumPath = entry.getValue();
+			}
+		}
+		assert appiumPath != null;
+
+			//Setup the service
 		System.out.println("Creating Appium Service");
 		File appiumLogFile = new File("screenshot/myAppiumLog.txt");
 		new FileOutputStream(appiumLogFile, false).close();
+
 		myAppiumService = new AppiumServiceBuilder()
-				.withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
+//				.withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
+				.withAppiumJS(new File(appiumPath))
 				.usingPort(myPort)
 				.withIPAddress("127.0.0.1")
 				.withLogFile(appiumLogFile)

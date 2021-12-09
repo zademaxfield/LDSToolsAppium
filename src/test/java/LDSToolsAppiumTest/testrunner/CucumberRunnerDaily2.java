5578b4a322b0848b81ed8e9d0c2a19bb1b6ea040
Zade Maxfield <zmaxfield@Zades-MacBook-Pro-2.local>
2021-12-09 14:34:12 -0700

    Fix for html test for Cucumber

--- src/test/java/LDSToolsAppiumTest/testrunner/CucumberRunnerDaily2.java
+++ src/test/java/LDSToolsAppiumTest/testrunner/CucumberRunnerDaily2.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@daily2",
         plugin = {
                 "pretty",
-                "html:src/test/java/Reports/cucumber-reports/cucumber-pretty",
+                "html:src/test/java/Reports/cucumber-reports/cucumber-pretty.html",
                 "json:src/test/java/Reports/cucumber-reports/CucumberTestReport.json",
                 "junit:src/test/java/Reports/cucumber-reports/CucumberTestReport.xml",
                 "rerun:src/test/java/Reports/cucumber-reports/rerun.txt"

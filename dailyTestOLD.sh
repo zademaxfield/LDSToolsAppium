fc220e3ec6c4596ede35d71ce52d36901d5efaa7
Zade Maxfield <zmaxfield@Zades-MacBook-Pro-2.local>
2021-12-14 09:12:54 -0700

    updates to daily tests

--- dailyTestOLD.sh
+++ dailyTestOLD.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd /Users/zmaxfield/Documents/workspace/qa-membertools-all
-/opt/homebrew/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/iOSDailyTests.xml
+/usr/local/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/iOSDailyTests.xml
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/target/surefire-reports/testng-results.xml /Users/zmaxfield/Documents/iOSDaily.xml
-/opt/homebrew/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/iOSDailyTestsCucumber.xml
+/usr/local/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/iOSDailyTestsCucumber.xml
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports/cucumber-reports1/CucumberTestReport.json /Users/zmaxfield/Documents/iOSDailyCucumber1.json
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports/cucumber-reports2/CucumberTestReport.json /Users/zmaxfield/Documents/iOSDailyCucumber2.json
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports/cucumber-reports3/CucumberTestReport.json /Users/zmaxfield/Documents/iOSDailyCucumber3.json
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports/cucumber-reports4/CucumberTestReport.json /Users/zmaxfield/Documents/iOSDailyCucumber4.json
-/opt/homebrew/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/AndroidDailyTestsMultiple.xml
+/usr/local/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/AndroidDailyTestsMultiple.xml
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/target/surefire-reports/testng-results.xml /Users/zmaxfield/Documents/AndroidDaily.xml
-/opt/homebrew/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/AndroidDailyTestsMultipleCucumber.xml
+/usr/local/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/AndroidDailyTestsMultipleCucumber.xml
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports/cucumber-reports1/CucumberTestReport.json /Users/zmaxfield/Documents/AndroidDailyCucumber1.json
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports/cucumber-reports2/CucumberTestReport.json /Users/zmaxfield/Documents/AndroidDailyCucumber2.json
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports/cucumber-reports3/CucumberTestReport.json /Users/zmaxfield/Documents/AndroidDailyCucumber3.json

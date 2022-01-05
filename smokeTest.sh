9d5367006bbc401d988ae68349181643552359de
Zade <zmaxfield@churchofjesuschrist.org>
2022-01-05 10:41:48 -0700

    Fixed cucumber test for smoke script

--- smokeTest.sh
+++ smokeTest.sh
@@ -2,7 +2,7 @@
 cd /Users/zmaxfield/Documents/workspace/qa-membertools-all
 /opt/homebrew/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/iOSSmokeTwoSim.xml
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/target/surefire-reports/testng-results.xml /Users/zmaxfield/Documents/iOSSmoke.xml
-/opt/homebrew/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/iOSDailyTestsCucumber.xml
+/opt/homebrew/bin/mvn clean test -DsuiteXmlFile=src/test/java/TestNG/iOSSmokeTwoSimCucumber.xml
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports/cucumber-reports1/CucumberTestReport.json /Users/zmaxfield/Documents/iOSSmokeCucumber1.json
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports/cucumber-reports2/CucumberTestReport.json /Users/zmaxfield/Documents/iOSSmokeCucumber2.json
 cp /Users/zmaxfield/Documents/workspace/qa-membertools-all/src/test/java/Reports/cucumber-reports3/CucumberTestReport.json /Users/zmaxfield/Documents/iOSSmokeCucumber3.json

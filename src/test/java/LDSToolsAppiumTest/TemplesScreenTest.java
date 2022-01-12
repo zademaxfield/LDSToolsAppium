5ecac647390086f2b13c9c05f7a5d88896eb4ab3
Zade <zmaxfield@churchofjesuschrist.org>
2022-01-12 10:53:11 -0700

    test

--- src/test/java/LDSToolsAppiumTest/TemplesScreenTest.java
+++ src/test/java/LDSToolsAppiumTest/TemplesScreenTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class TemplesScreenTest extends BaseDriver {
 
-    @Test (groups = {"all2", "all", "smoke", "smoke2", "daily", "daily3"})
+    @Test (groups = {"all2", "all", "smoke", "smoke2", "daily", "daily3", "jft"})
     public void templeSimple() throws Exception {
         String pageSource;
         HelperMethods myHelper = new HelperMethods();
@@ -45,7 +45,7 @@ public class TemplesScreenTest extends BaseDriver {
     }
 
 
-    @Test (groups= {"all", "all1", "daily", "daily4", "jft"})
+    @Test (groups= {"all", "all1", "daily", "daily4"})
     public void templeRecommendReminder25Days() throws Exception {
         String pageSource;
         HelperMethods myHelper = new HelperMethods();

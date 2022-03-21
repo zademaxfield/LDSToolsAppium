5e45b5d00af6b08fbfc1108daf38007d0bb9ec50
Zade <zmaxfield@churchofjesuschrist.org>
2022-03-21 14:13:10 -0600

    Update iOS version

--- src/main/java/LDSToolsAppium/BaseDriver.java
+++ src/main/java/LDSToolsAppium/BaseDriver.java
@@ -605,7 +605,7 @@ public class BaseDriver extends AbstractTestNGCucumberTests {
             capabilities.setCapability("appium:newCommandTimeout", 600);
             capabilities.setCapability("appium:app", app.getAbsolutePath());
             capabilities.setCapability("launchTimeout", 900000);
-            capabilities.setCapability("appium:platformVersion", "15.2");
+            capabilities.setCapability("appium:platformVersion", "15.4");
             capabilities.setCapability("nativeInstrumentsLib", false);
             capabilities.setCapability("appium:clearSystemFiles", true);
             //capabilities.setCapability("allowTouchIdEnroll", true);
